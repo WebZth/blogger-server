e13072273a7de3e8f6c07f3fcde57efbfe984f15
utsavojha <utsav.ojha@hotmail.com>
2021-07-02 20:59:07 +0530

    added contentSecurityPlicy to helemt setting app.js

--- express.js
+++ express.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(express.static("build"));
 app.use("/", authRoutes);
